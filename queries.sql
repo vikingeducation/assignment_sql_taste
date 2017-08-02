9b035913d9fa9b14095f07bb20da45257c427871
Alex-Willenbrink <awillenbrink@gmail.com>
2017-08-01 20:33:52 -0500

    added numbers

--- queries.sql
+++ queries.sql
@@ -1,11 +1,14 @@
+--  tutorial.us_house_units
+-- 1.
 -- SELECT *
 --   FROM tutorial.us_housing_units
 --   LIMIT 10
 
--- COME BACK LATER
+-- 2.
 -- SELECT midwest
 --   FROM tutorial.us_housing_units
 
+-- 3.
 -- SELECT south,
 --       west,
 --       midwest,
@@ -13,6 +16,7 @@
 -- FROM tutorial.us_housing_units
 -- WHERE month = 12 AND year >= 1985
 
+-- 4.
 -- SELECT south,
 --       west,
 --       midwest,
@@ -20,42 +24,44 @@
 --     FROM tutorial.us_housing_units
 --     WHERE month > 6 AND year >= 1990
 
+-- 5.
 -- SELECT *
 --   FROM tutorial.us_housing_units
 --   WHERE south > 30
 
-
+-- 6.
 -- SELECT SUM(south) AS south_sum,
 --       SUM(west) AS west_sum,
 --       SUM(northeast) AS northeast_sum,
 --       SUM(midwest) AS midwest_sum
 --     FROM tutorial.us_housing_units
 
+-- 7.
 -- SELECT *
 --     FROM tutorial.us_housing_units
 --     WHERE (south + west + northeast + midwest) > 70
 
+-- 8.
 -- SELECT *
 --   FROM tutorial.us_housing_units
 --   WHERE (south + west + northeast + midwest) BETWEEN 50 AND 80
 
 
-
+-- 9.
 -- SELECT south AS south_avg,
 --       AVG(west) AS west_avg,
 --       AVG(northeast) AS northeast_avg,
 --       AVG(midwest) AS midwest_avg
 --     FROM tutorial.us_housing_units
 
+-- 10.
 -- SELECT *
 --   FROM tutorial.us_housing_units
 --   WHERE south > (west + northeast + midwest)
 
--- SELECT (south + west + northeast + midwest) AS total
---   WHERE year
 
-
-
+-- 11.
+-- VERSION 1
 -- SELECT SUM(south) / (SUM(south) + SUM(west) + SUM(northeast) + SUM(midwest)) AS south_avg,
 --       SUM(west) / (SUM(south) + SUM(west) + SUM(northeast) + SUM(midwest)) AS west_avg,
 --     SUM(northeast) / (SUM(south) + SUM(west) + SUM(northeast) + SUM(midwest)) AS northeast_avg,
@@ -63,7 +69,7 @@
 --     FROM tutorial.us_housing_units
 --     WHERE year >= 1990
 
-
+-- VERSION 2
 -- SELECT
 --         sum_south / sum_total AS percent_south,
 --         sum_west / sum_total AS percent_west,
@@ -79,43 +85,50 @@
 --         WHERE year >= 1990
 --         ) AS sum_total
 
-
+-- tutorial.billboard_top_100_year_end
+-- 1.
   -- SELECT *
   --   FROM tutorial.billboard_top_100_year_end
   --   WHERE artist = 'Elvis Presley'
 
+-- 2.
 -- SELECT *
 --   FROM tutorial.billboard_top_100_year_end
 --   WHERE artist ILIKE '%tony%'
 
+-- 3.
 -- SELECT *
 --   FROM tutorial.billboard_top_100_year_end
 --   WHERE song_name ILIKE '%lov%' OR song_name ILIKE '%luv%'
 
+-- 4.
 -- SELECT *
 --   FROM tutorial.billboard_top_100_year_end
 --   WHERE artist ILIKE 'A%'
 
+-- 5.
 -- SELECT *
 --   FROM tutorial.billboard_top_100_year_end
 --   WHERE year BETWEEN 1960 AND 1969
 --   AND year_rank < 4
 
+-- 6.
 -- SELECT *
 --     FROM tutorial.billboard_top_100_year_end
 --     WHERE artist IN ('Elvis Presley', 'Rolling Stones', 'Van Halen')
 
-
-
+-- 7.
 -- SELECT DISTINCT artist
 --       FROM  tutorial.billboard_top_100_year_end
 --       WHERE artist IS NOT NULL
 
+-- 8.
 -- SELECT MAX(
 --   (SELECT artist, COUNT(*) AS artist_hits
 --   FROM  tutorial.billboard_top_100_year_end
 --   GROUP BY artist
 --   WHERE artist IS NOT NULL) AS artist_top_100
+
 
 
 -- 12.
